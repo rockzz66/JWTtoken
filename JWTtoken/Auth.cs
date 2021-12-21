7f737ee8206decc86e65f4a03a2b03aa3b7993a9
Sankeerth <sankeerthyadav66@gmail.com>
2021-12-21 16:38:25 +0530

    Add project files.

---
+++ JWTtoken/Auth.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace JWTtoken
+{
+    public class Auth : IJwtAuth
+    {
+        private readonly string username = "Sankeerth";
+        private readonly string password = "123456";
+        private readonly string key;
+        public Auth(string key)
+        {
+            this.key = key;
+        }
+        public string Authentication(string username, string password)
+        {
+            if(!(username.Equals(username)|| password.Equals(password)))
+            {
+                return null;
+            }
+            //Token Handler
+            var tokenHandler = new JwtSecurityTokenHandler();
+            //Creating private Key to Encrypt
+            var tokenKey = System.Text.Encoding.ASCII.GetBytes(key);
+            //setting cryptographic algorithm
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(
+                    new Claim[]
+                    {
+                        new Claim(ClaimTypes.Name, username)
+                    }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials=new SigningCredentials(
+                    new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);//create token
+            return tokenHandler.WriteToken(token);//return token from method 
+        }
+        //public string Authentication(string username, string password)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
---
+++ JWTtoken/Controllers/MembersController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+
+using JWTtoken;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JWTtokenControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController : ControllerBase
+    {
+        private readonly IJwtAuth jwtAuth;
+        private List<Member1> OneMember = new List<Member1>()
+        {
+            new Member1(){ Id = 1, Name = "Sankeerth" },
+            new Member1() {Id=2, Name="Nitya" },
+            new Member1(){ Id = 3, Name = "pankaj" } };
+
+        // GET: api/<MembersController>
+        public MembersController(IJwtAuth jwtAuth)
+        {
+            this.jwtAuth = jwtAuth;
+        }
+
+        [HttpGet]
+        public IEnumerable<Member1> AllMembers() => (IEnumerable<Member1>)OneMember;
+
+        // GET api/<MembersController>/5
+        [HttpGet("{id}")]
+        public Member1 MemberByid(int id) =>OneMember.Find(s => s.Id == id);
+        
+
+// POST api/<MembersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MembersController>/5
+        [AllowAnonymous]
+        // POST api/<MembersController>
+        [HttpPost("authentication")]
+        public IActionResult Authentication([FromBody] UserCredential userCredential)
+        {
+            var token = jwtAuth.Authentication(userCredential.UserName, userCredential.Password);
+            if (token == null)
+                return Unauthorized();
+            return Ok(token);
+        }
+    }
+}
+//IActionResult Ok(object token)
+//{
+//    throw new NotImplementedException();
+//}
